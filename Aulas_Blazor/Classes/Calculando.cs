a48fb6af4dca9cc1efa4c3eab884a87c08a217ae
Lucas Augusto <lucas_lapada@hotmail.com>
2023-05-25 18:01:59 -0300

    Implementando as classes e entidades

---
+++ Aulas_Blazor/Classes/Calculando.cs
@@ -0,0 +1,32 @@
+﻿namespace Aulas_Blazor.Classes
+{
+    public class Calculando
+    {
+        public static double Soma(int n1, int n2)
+        {
+
+            double resultado = n1 + n2;
+            return resultado; ;
+        }
+
+        public static double Multiplica(int n1, int n2)
+        {
+
+            double resultado = n1 * n2;
+            return resultado;
+        }
+        public static double Subritai(int n1, int n2)
+        {
+
+            double resultado = n1 - n2;
+            return resultado;
+        }
+
+        public static double Divide(int n1, int n2)
+        {
+
+            double resultado = n1 / n2;
+            return resultado;
+        }
+    }
+}
---
+++ Aulas_Blazor/Shared/Entities/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace Aulas_Blazor.Shared.Entities
+{
+    public class Produto
+    {
+        public string  Nome { get; set; }
+        public string Tipo { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
